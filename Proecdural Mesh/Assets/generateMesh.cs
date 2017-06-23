f6fdddc8165c228b7b50cba31774ec0d106f5ef7
wmfarooqi05 <wmfarooqi05@gmail.com>
2017-06-23 11:28:03 +0500

    This is code for Procedural Generation
    This project contain code for creating triangle and square on runtime. It also have code for drawing things on Gizmos

---
+++ Proecdural Mesh/Assets/generateMesh.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generateMesh : MonoBehaviour {
+    private float speed;
+    Mesh mesh;
+
+
+    // Use this for initialization
+    void Start () {
+
+        // assign points
+        mesh = new Mesh();
+        
+        Vector3[] vertices = new Vector3[3];
+        
+        vertices[0] = new Vector3(0, 1, 0);
+        vertices[1] = new Vector3(1,0, 0);
+        vertices[2] = new Vector3(0, 0, 0);
+
+        /*
+        vertices[0] = new Vector3(-1, -1, 0);
+        vertices[1] = new Vector3(0, .8f, 0);
+        vertices[2] = new Vector3(1, -1, 0);
+        */
+        mesh.vertices = vertices;
+        
+        // assign triangles
+        int[] triangles =  new int[3] { 0, 1, 2 };
+        mesh.triangles = triangles;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        
+        // Initializing Normals
+        Vector3[] normals = new Vector3[3];
+        normals[0] = Vector3.forward;
+        normals[1] = Vector3.forward;
+        normals[2] = Vector3.forward;
+
+        // Assign Normals
+        mesh.normals = normals;
+
+        /*
+        Vector2[] uvs = new Vector2[3];
+        uvs[0] = new Vector2(0, 0);
+        uvs[1] = new Vector2(0, 1);
+        uvs[2] = new Vector2(1, 0);
+        */
+//        uvs[3] = new Vector2(1, 1);
+        //mesh.uv = uvs;
+
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Creating Colors
+        /*
+        Color32[] colors = new Color32[3];
+
+        float offset = Time.time * speed;
+
+        float color = Color.white.GetHashCode() + offset;
+        Material mat = GetComponent<Material>();
+
+        Search for this
+        ColorFromHue()
+
+        
+        colors[0] = Color.Lerp(Color.white,Color.red, offset);
+        colors[1] = Color.Lerp(Color.white, Color.blue, offset);
+        colors[2] = Color.Lerp(Color.white, Color.green, offset);
+        */
+        //mesh.colors32 = colors;
+        // = colors;
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3[] vertices = mesh.vertices;
+
+        for(int i=0; i<vertices.Length; i++)
+        {
+            Gizmos.DrawWireSphere(center: vertices[i] + transform.position, radius: .1f);
+        }
+    }
+
+}
---
+++ Proecdural Mesh/Assets/generateTrinagle.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class generateTrinagle : MonoBehaviour {
+
+    [SerializeField]
+    const int width = 4;
+    const int height = 4;
+
+    Mesh mesh;
+
+    // Use this for initialization
+    void Start () {
+
+        mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[(width * height) * 4];
+
+        int vertexIndex1 = 0;
+        for (int x=0; x<width; x++)
+        {
+            for (int y=0; y< height; y++)
+            {
+                
+                vertices[vertexIndex1++] = new Vector3(-1 + x, -1 + y);
+                vertices[vertexIndex1++] = new Vector3(1 + x, -1 + y);
+                vertices[vertexIndex1++] = new Vector3(1 + x, 1 + y);
+                vertices[vertexIndex1++] = new Vector3(-1 + x, 1 + y);
+                //vertexIndex1 += 4;
+            }
+        }
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[(width - 1) * (height - 1) * 6]; // 6 for 6 sides of triangle in a cube
+
+        int triangleIndex = 0;
+
+        for (int x = 0; x < width - 1; x++)
+        {
+            for (int y = 0; y < height - 1; y++)
+            {
+                int vertexIndex = x * width + y;
+
+                triangles[triangleIndex + 0] = vertexIndex;
+                triangles[triangleIndex + 1] = vertexIndex + width;
+                triangles[triangleIndex + 2] = vertexIndex + width + 1;
+                triangles[triangleIndex + 3] = vertexIndex;
+                triangles[triangleIndex + 4] = vertexIndex + width + 1;
+                triangles[triangleIndex + 5] = vertexIndex + 1;
+            
+                triangleIndex += 6;
+
+            }
+        }
+
+        
+        mesh.triangles = triangles;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+
+    void OnDrawGizmos()
+    {
+        Vector3[] vertices = mesh.vertices;
+
+//        Debug.Log("position in gizmo: " + transform.position);
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawWireSphere(vertices[i] + transform.position, radius: .1f);
+        }
+    }
+
+}
